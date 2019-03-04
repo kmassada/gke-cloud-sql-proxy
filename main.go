61fdc2928cbd25edd471a346b367da57148de362
Kenneth Massada <kmassada@gmail.com>
2019-03-04 16:19:34 +0000

    log instead of panic

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -13,7 +14,7 @@ func main() {
 	fmt.Print(connectstring)
 	db, err := sql.Open("mysql", connectstring)
 	if err != nil {
-		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+		log.Fatal(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
 	}
 	defer db.Close()
 
@@ -22,20 +23,20 @@ func main() {
 	fmt.Print("pinging")
 	if err != nil {
 		fmt.Print("ping hung")
-		panic(err.Error()) // proper error handling instead of panic in your app
+		log.Fatal(err.Error()) // proper error handling instead of panic in your app
 	}
 
 	// Prepare statement for inserting data
 	stmtIns, err := db.Prepare("INSERT INTO squareNum VALUES( ?, ? )") // ? = placeholder
 	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
+		log.Fatal(err.Error()) // proper error handling instead of panic in your app
 	}
 	defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
 
 	// Prepare statement for reading data
 	stmtOut, err := db.Prepare("SELECT squareNumber FROM squarenum WHERE number = ?")
 	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
+		log.Fatal(err.Error()) // proper error handling instead of panic in your app
 	}
 	defer stmtOut.Close()
 
@@ -43,7 +44,7 @@ func main() {
 	for i := 0; i < 25; i++ {
 		_, err = stmtIns.Exec(i, (i * i)) // Insert tuples (i, i^2)
 		if err != nil {
-			panic(err.Error()) // proper error handling instead of panic in your app
+			log.Fatal(err.Error()) // proper error handling instead of panic in your app
 		}
 	}
 
@@ -52,14 +53,14 @@ func main() {
 	// Query the square-number of 13
 	err = stmtOut.QueryRow(13).Scan(&squareNum) // WHERE number = 13
 	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
+		log.Fatal(err.Error()) // proper error handling instead of panic in your app
 	}
 	fmt.Printf("The square number of 13 is: %d", squareNum)
 
 	// Query another number.. 1 maybe?
 	err = stmtOut.QueryRow(1).Scan(&squareNum) // WHERE number = 1
 	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
+		log.Fatal(err.Error()) // proper error handling instead of panic in your app
 	}
 	fmt.Printf("The square number of 1 is: %d", squareNum)
 }
