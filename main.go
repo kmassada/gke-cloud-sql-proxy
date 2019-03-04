de5371cc8f4daa051960ed3ffc5c6a6e876c8701
Kenneth Massada <kmassada@gmail.com>
2019-03-04 17:22:09 +0000

    attempt to make container go forever

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/VividCortex/mysqlerr"
 	"github.com/go-sql-driver/mysql"
@@ -14,30 +16,30 @@ func main() {
 	connectstring := os.Getenv("CLOUDSQL_DB_USER") + ":" + os.Getenv("CLOUDSQL_DB_PASSWORD") + "@tcp(" + os.Getenv("CLOUDSQL_DB_HOST") + ")/cloudsqlclient"
 	db, err := sql.Open("mysql", connectstring)
 	if err != nil {
-		fmt.Print(connectstring)
+		fmt.Println(connectstring)
 		log.Fatal(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
 	}
 	defer db.Close()
 
 	// Open doesn't open a connection. Validate DSN data:
 	err = db.Ping()
-	fmt.Print("pinging")
+	fmt.Println("pinging")
 	if err != nil {
-		fmt.Print("ping hung")
-		log.Fatal(err.Error()) // proper error handling instead of panic in your app
+		fmt.Println("ping hung")
+		log.Fatal(err.Error())
 	}
 
 	// Prepare statement for inserting data
 	stmtIns, err := db.Prepare("INSERT INTO squareNum VALUES( ?, ? )") // ? = placeholder
 	if err != nil {
-		log.Fatal(err.Error()) // proper error handling instead of panic in your app
+		log.Fatal(err.Error())
 	}
 	defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
 
 	// Prepare statement for reading data
 	stmtOut, err := db.Prepare("SELECT squareNumber FROM squareNum WHERE number = ?")
 	if err != nil {
-		log.Fatal(err.Error()) // proper error handling instead of panic in your app
+		log.Fatal(err.Error())
 	}
 	defer stmtOut.Close()
 
@@ -50,24 +52,27 @@ func main() {
 					log.Printf(err.Error())
 				}
 			} else {
-				log.Fatal(err.Error()) // proper error handling instead of panic in your app
+				log.Fatal(err.Error())
 			}
 		}
 	}
 
-	var squareNum int // we "scan" the result in here
+	for {
+		// Seed before random int
+		rand.Seed(time.Now().UTC().UnixNano())
 
-	// Query the square-number of 13
-	err = stmtOut.QueryRow(13).Scan(&squareNum) // WHERE number = 13
-	if err != nil {
-		log.Fatal(err.Error()) // proper error handling instead of panic in your app
-	}
-	fmt.Printf("The square number of 13 is: %d", squareNum)
+		var squareNum int // we "scan" the result in here
 
-	// Query another number.. 1 maybe?
-	err = stmtOut.QueryRow(1).Scan(&squareNum) // WHERE number = 1
-	if err != nil {
-		log.Fatal(err.Error()) // proper error handling instead of panic in your app
+		// Query the square-number of 13
+		err = stmtOut.QueryRow(randInt(0, 24)).Scan(&squareNum) // WHERE number = 13
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		fmt.Printf("The square number of 13 is: %d", squareNum)
+
+		time.Sleep(time.Second)
 	}
-	fmt.Printf("The square number of 1 is: %d", squareNum)
+}
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
 }
